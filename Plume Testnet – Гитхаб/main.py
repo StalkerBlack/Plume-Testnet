37f88dfcbf9c562477eddbf0f371becd17a9eedd
StalkerBlack <59935250+StalkerBlack@users.noreply.github.com>
2024-09-02 09:37:52 +0300

    main.py

--- Plume Testnet – Гитхаб/main.py
+++ Plume Testnet – Гитхаб/main.py
@@ -37,67 +37,6 @@ print('************************************************************************'
 """
 
 
-#  FAUCET ETH, GOON
-for account in ACCOUNTS:
-    sleep(33, 368)
-    try:
-        print_with_time(f"\033[33m Запрашиваем токены для {account['name']}\033[0m")
-
-        # Создание клиента для каждого аккаунта
-        client = Client(private_key=account['private_key'], network=Plume)
-
-        if not client.w3.is_connected():
-            print_with_time(f"\033[31m Не удалось подключиться: {account['name']}\033[0m")
-            continue
-
-        captcha_token = token_extraction()
-        wallet = account['address']
-        # Подготовка payload для запроса
-        payload = {
-            "walletAddress": wallet,
-            "token": "ETH",
-            "verified": captcha_token,
-        }
-
-
-        proxies = account['proxies']
-        headers = account['headers']
-        # Выполнение POST-запроса к API
-        response = requests.post(
-            "https://faucet.plumenetwork.xyz/api/faucet",
-            json=payload,
-            proxies=proxies,
-            headers=headers,
-        )
-
-        data = response.json()
-        contract = faucet(client)
-
-        token = "ETH"
-        # token = "GOON"
-        salt = data["salt"]
-        signature = data["signature"]
-
-        tx = contract.functions.getToken(token, salt, signature)._encode_transaction_data()
-
-        tx_hash = client.send_transaction(
-            to='0x075e2D02EBcea5dbcE6b7C9F3D203613c0D5B33B',
-            data=tx
-            )
-
-        if tx_hash:
-            print_with_time(f"\033[32m Токены '{token}' для {account['name']} успешно запрошены!!! Hash: {tx_hash.hex()}\033[0m")
-        else:
-            print_with_time(f"\033[31m Не удалось запросить токены с {account['name']}\033[0m")
-
-    except Exception as e:
-        print_with_time(f"\033[31m Возникла ошибка {account['name']}: {str(e)}\033[0m")
-print('************************************************************************')
-print_with_time(f'\033[32m Токены "{token}" с крана на всех аккаунтах запрошены\033[0m')
-print('************************************************************************')
-
-
-
 """# Голосовалка
 for account in ACCOUNTS:
     sleep(33, 368)
