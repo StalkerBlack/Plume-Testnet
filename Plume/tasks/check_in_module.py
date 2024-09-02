e86c15727996371c9d4252c8f0dd11d69035fade
Георгий Исак <isak.georgiy@mail.ru>
2024-09-02 22:17:24 +0300

    tiny corrections

--- Plume/tasks/check_in_module.py
+++ Plume/tasks/check_in_module.py
@@ -20,16 +20,14 @@ class CheckInWorker:
         PROXY_CONTRACT_ADDRESS = '0x8Dc5b3f1CcC75604710d9F464e3C5D2dfCAb60d8'
         IMPLEMENTATION_CONTRACT_ADDRESS = '0xC2b6fe9C66cB72543f7434bdC610A20fD8F6038B'
 
-        web3 = self.client.w3
-
-        implementation_contract = web3.eth.contract(address=IMPLEMENTATION_CONTRACT_ADDRESS, abi=self.abi)
+        implementation_contract = self.client.w3.eth.contract(address=IMPLEMENTATION_CONTRACT_ADDRESS, abi=self.abi)
 
         check_in_data = implementation_contract.functions.checkIn()._encode_transaction_data()
 
-        tx_hash = self.client.w3.eth.send_transaction(
+        tx_hash = self.client.send_transaction(
             to=PROXY_CONTRACT_ADDRESS,
             data=check_in_data
         )
-        logger.info(f"Tx hash for check in: {tx_hash.hex()}")
+        logger.info(f"Tx hash for check in: {tx_hash}")
         return tx_hash
 
