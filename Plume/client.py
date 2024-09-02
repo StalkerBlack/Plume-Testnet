1507f246ad23e52626528b4bb233e111bf9e8059
Георгий Исак <isak.georgiy@mail.ru>
2024-09-02 22:18:18 +0300

    add processing exception and tiny corrections

--- Plume/client.py
+++ Plume/client.py
@@ -1,5 +1,6 @@
 from loguru import logger
 from web3 import Web3
+from web3.exceptions import ContractCustomError
 from typing import Optional, Dict
 
 
@@ -45,21 +46,21 @@ class Client:
         log_message = f"Инициализирован {self.number} клиент | Адрес {self.address} | Прокси: {self.proxy}"
         logger.info(log_message)
 
-    @staticmethod
-    def get_max_priority_fee_per_gas(w3: Web3, block: dict) -> int:
+
+    def get_max_priority_fee_per_gas(self, block: dict) -> int:
         block_number = block['number']
-        latest_block_transaction_count = w3.eth.get_block_transaction_count(block_number)
+        latest_block_transaction_count = self.w3.eth.get_block_transaction_count(block_number)
         max_priority_fee_per_gas_lst = []
         for i in range(latest_block_transaction_count):
             try:
-                transaction = w3.eth.get_transaction_by_block(block_number, i)
+                transaction = self.w3.eth.get_transaction_by_block(block_number, i)
                 if 'maxPriorityFeePerGas' in transaction:
                     max_priority_fee_per_gas_lst.append(transaction['maxPriorityFeePerGas'])
-            except Exception:
+            except Exception as error:
                 continue
 
         if not max_priority_fee_per_gas_lst:
-            max_priority_fee_per_gas = w3.eth.max_priority_fee
+            max_priority_fee_per_gas = self.w3.eth.max_priority_fee
         else:
             max_priority_fee_per_gas_lst.sort()
             max_priority_fee_per_gas = max_priority_fee_per_gas_lst[len(max_priority_fee_per_gas_lst) // 2]
@@ -70,15 +71,15 @@ class Client:
             to,
             data=None,
             from_=None,
-            increase_gas=1.125,
+            increase_gas: float = 1.125,
             value=None,
             max_priority_fee_per_gas: Optional[int] = None,
             max_fee_per_gas: Optional[int] = None
-    ):
+        ):
         if not from_:
             from_ = self.address
 
-        tx_params = {
+        tx_params: dict = {
             'chainId': self.w3.eth.chain_id,
             'nonce': self.w3.eth.get_transaction_count(self.address),
             'from': Web3.to_checksum_address(from_),
@@ -88,13 +89,11 @@ class Client:
             tx_params['data'] = data
 
         if self.network.eip1559_tx:
-            w3 = Web3(provider=Web3.HTTPProvider(endpoint_uri=self.network.rpc))
-
-            last_block = w3.eth.get_block('latest')
+            last_block = self.w3.eth.get_block('latest')
             base_fee = int(last_block['baseFeePerGas'] * increase_gas)
             
             if not max_priority_fee_per_gas:
-                max_priority_fee_per_gas = Client.get_max_priority_fee_per_gas(w3=w3, block=last_block)
+                max_priority_fee_per_gas = self.get_max_priority_fee_per_gas(block=last_block)
             if not max_fee_per_gas or max_fee_per_gas < base_fee:
                 max_fee_per_gas = base_fee + max_priority_fee_per_gas
             
@@ -109,8 +108,10 @@ class Client:
 
         try:
             tx_params['gas'] = int(self.w3.eth.estimate_gas(tx_params) * increase_gas)
-        except Exception as err:
-            print(f'{self.address} | Transaction failed | {err}')
+
+        except ContractCustomError as error:
+            logger.info(f"Check In уже ранее был выполнен на кошельке {self.address} | {error}")
+            # print(f'{self.address} | Transaction failed | {err}')
             return None
 
         sign = self.w3.eth.account.sign_transaction(tx_params, self.private_key)
