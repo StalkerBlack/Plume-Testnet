0778357bc6c1e7b781523314b34b045b048df487
Георгий Исак <isak.georgiy@mail.ru>
2024-09-03 19:47:15 +0300

    add sleep mode

--- Plume/settings.py
+++ Plume/settings.py
@@ -5,7 +5,12 @@ from models import Plume
 
 GLOBAL_NETWORK         Сеть, в которой работаем (по умолчанию, Plume Network)
 
+SLEEP_MODE = False     True или False | Включает сон после каждого модуля и аккаунта
+
+SLEEP_TIME = (20, 35)   (минимум, максимум) секунд | Время сна между модулями.
 """
 
 
-GLOBAL_NETWORK = Plume
+GLOBAL_NETWORK = Plume            # Сеть, в которой работаем
+SLEEP_MODE = False               # True или False | Включает сон после каждого модуля и аккаунта
+SLEEP_TIME = (20, 35)           # (минимум, максимум) секунд | Время сна между модулями.
