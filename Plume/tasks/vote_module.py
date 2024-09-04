7cd91a04d4fd4765f9b4435bb1cde1a86c1a7ec2
Георгий Исак <isak.georgiy@mail.ru>
2024-09-04 11:15:38 +0300

    change vote module

--- Plume/tasks/vote_module.py
+++ Plume/tasks/vote_module.py
@@ -1,72 +1,39 @@
+import asyncio
 import random
 
 
-from ..utils import read_json
-from ..data.config import VOTE
+from loguru import logger
+
+from Plume.client import Client
+from Plume.data.config import VOTE
+from Plume.utils import read_json
 
 
+class VoteWorker:
+    def __init__(self, client: Client):
+        self.client: Client = client
+        self.abi = read_json(VOTE)
 
-def vote_1(client):
-    PROXY_CONTRACT_ADDRESS = '0xbd06be7621be8f92101bf732773e539a4daf7e3f'
-    IMPLEMENTATION_CONTRACT_ADDRESS = '0x92D8e70879ba9Ad9C0Cf540F48FbdD692D9CE086'
-    IMPLEMENTATION_ABI = read_json(VOTE)
+    async def vote(self):
+        PROXY_CONTRACT_ADDRESS = '0xbd06be7621be8f92101bf732773e539a4daf7e3f'
+        IMPLEMENTATION_CONTRACT_ADDRESS = '0x92D8e70879ba9Ad9C0Cf540F48FbdD692D9CE086'
 
-    web3 = client.w3
+        implementation_contract = self.client.w3.eth.contract(address=IMPLEMENTATION_CONTRACT_ADDRESS, abi=self.abi)
 
-    implementation_contract = web3.eth.contract(address=IMPLEMENTATION_CONTRACT_ADDRESS, abi=IMPLEMENTATION_ABI)
+        lst_data = [18, 21]
+        data = random.choice(lst_data)
 
-    lst_data = [23, 33, 26]
-    data = random.choice(lst_data)
+        vote_data = implementation_contract.functions.vote(data)._encode_transaction_data()
 
-    check_in_data = implementation_contract.functions.vote(data)._encode_transaction_data()
+        for _ in range(3):
+            tx_hash = await self.client.send_transaction(
+                to=PROXY_CONTRACT_ADDRESS,
+                data=vote_data
+            )
+            if tx_hash:
+                logger.success(f"Успешно проголосовали!\nХэш транзакции: {self.client.network.explorer + tx_hash} | Адрес: {self.client.address}")
+            else:
+                logger.info(f"Не удалось проголосовать на {self.client.number} кошельке: {self.client.address}")
 
-    tx_hash = client.send_transaction(
-        to=PROXY_CONTRACT_ADDRESS,
-        data=check_in_data
-    )
-
-    return tx_hash
-
-def vote_2(client):
-    PROXY_CONTRACT_ADDRESS = '0xbd06be7621be8f92101bf732773e539a4daf7e3f'
-    IMPLEMENTATION_CONTRACT_ADDRESS = '0x92D8e70879ba9Ad9C0Cf540F48FbdD692D9CE086'
-    IMPLEMENTATION_ABI = read_json(VOTE)
-
-    web3 = client.w3
-
-    implementation_contract = web3.eth.contract(address=IMPLEMENTATION_CONTRACT_ADDRESS, abi=IMPLEMENTATION_ABI)
-
-    lst_data = [24, 31, 27]
-    data = random.choice(lst_data)
-
-    check_in_data = implementation_contract.functions.vote(data)._encode_transaction_data()
-
-    tx_hash = client.send_transaction(
-        to=PROXY_CONTRACT_ADDRESS,
-        data=check_in_data
-    )
-
-    return tx_hash
-
-
-def vote_3(client):
-    PROXY_CONTRACT_ADDRESS = '0xbd06be7621be8f92101bf732773e539a4daf7e3f'
-    IMPLEMENTATION_CONTRACT_ADDRESS = '0x92D8e70879ba9Ad9C0Cf540F48FbdD692D9CE086'
-    IMPLEMENTATION_ABI = read_json(VOTE)
-
-    web3 = client.w3
-
-    implementation_contract = web3.eth.contract(address=IMPLEMENTATION_CONTRACT_ADDRESS, abi=IMPLEMENTATION_ABI)
-
-    lst_data = [20, 32, 25]
-    data = random.choice(lst_data)
-
-    check_in_data = implementation_contract.functions.vote(data)._encode_transaction_data()
-
-    tx_hash = client.send_transaction(
-        to=PROXY_CONTRACT_ADDRESS,
-        data=check_in_data
-    )
-
-    return tx_hash
-
+            # Спим 2 минуты
+            await asyncio.sleep(120)
