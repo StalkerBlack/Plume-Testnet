ceaf5444f1ddb01c906549a83adcf52a5e18e8d1
Георгий Исак <isak.georgiy@mail.ru>
2024-09-06 21:44:19 +0300

    made annotations for check in module

--- Plume/tasks/check_in_module.py
+++ Plume/tasks/check_in_module.py
@@ -1,7 +1,6 @@
-import json
-
-
 from loguru import logger
+from web3 import Web3
+from web3.eth.async_eth import AsyncContract
 
 
 from Plume.client import Client
@@ -10,24 +9,35 @@ from Plume.data.config import CHECK_IN_ABI
 
 
 class CheckInWorker:
-    def __init__(self,
-                 client: Client
-                 ):
+    def __init__(self, client: Client):
         self.client: Client = client
         self.abi = read_json(CHECK_IN_ABI)
 
     async def check_in(self):
-        PROXY_CONTRACT_ADDRESS = '0x8Dc5b3f1CcC75604710d9F464e3C5D2dfCAb60d8'
-        IMPLEMENTATION_CONTRACT_ADDRESS = '0xC2b6fe9C66cB72543f7434bdC610A20fD8F6038B'
+        PROXY_CONTRACT_ADDRESS = Web3.to_checksum_address(
+            "0x8Dc5b3f1CcC75604710d9F464e3C5D2dfCAb60d8"
+        )
+        IMPLEMENTATION_CONTRACT_ADDRESS = Web3.to_checksum_address(
+            "0xC2b6fe9C66cB72543f7434bdC610A20fD8F6038B"
+        )
 
-        implementation_contract = self.client.w3.eth.contract(address=IMPLEMENTATION_CONTRACT_ADDRESS, abi=self.abi)
+        implementation_contract: AsyncContract = self.client.w3.eth.contract(
+            address=IMPLEMENTATION_CONTRACT_ADDRESS, abi=self.abi
+        )
 
-        check_in_data = implementation_contract.functions.checkIn()._encode_transaction_data()
+        check_in_data = (
+            implementation_contract.functions.checkIn()._encode_transaction_data()
+        )
 
         tx_hash = await self.client.send_transaction(
-            to=PROXY_CONTRACT_ADDRESS,
-            data=check_in_data
+            to=PROXY_CONTRACT_ADDRESS, data=check_in_data
         )
-        logger.info(f"Хэш транзакции: {self.client.network.explorer + tx_hash} | Адрес: {self.client.address}")
-        return tx_hash
-
+        if tx_hash:
+            logger.info(
+                f"Хэш транзакции: {self.client.network.explorer + tx_hash} | Адрес: {self.client.address}"
+            )
+        else:
+            logger.error(
+                f"Не удалось выполнить check in на {self.client.number} кошелька: {self.client.address}"
+            )
+        return
