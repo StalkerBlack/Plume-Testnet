b6c3c3d6e0a73434a1f39f7142d86bcfc7197ef3
Георгий Исак <isak.georgiy@mail.ru>
2024-09-06 21:43:14 +0300

    made annotations for vote module

--- Plume/tasks/vote_module.py
+++ Plume/tasks/vote_module.py
@@ -3,6 +3,8 @@ import random
 
 
 from loguru import logger
+from web3 import Web3
+from web3.eth.async_eth import ChecksumAddress, AsyncContract
 
 from Plume.client import Client
 from Plume.data.config import VOTE
@@ -15,25 +17,36 @@ class VoteWorker:
         self.abi = read_json(VOTE)
 
     async def vote(self):
-        PROXY_CONTRACT_ADDRESS = '0xbd06be7621be8f92101bf732773e539a4daf7e3f'
-        IMPLEMENTATION_CONTRACT_ADDRESS = '0x92D8e70879ba9Ad9C0Cf540F48FbdD692D9CE086'
+        PROXY_CONTRACT_ADDRESS: ChecksumAddress = Web3.to_checksum_address(
+            "0xbd06be7621be8f92101bf732773e539a4daf7e3f"
+        )
+        IMPLEMENTATION_CONTRACT_ADDRESS: ChecksumAddress = Web3.to_checksum_address(
+            "0x92D8e70879ba9Ad9C0Cf540F48FbdD692D9CE086"
+        )
 
-        implementation_contract = self.client.w3.eth.contract(address=IMPLEMENTATION_CONTRACT_ADDRESS, abi=self.abi)
+        implementation_contract: AsyncContract = self.client.w3.eth.contract(
+            address=IMPLEMENTATION_CONTRACT_ADDRESS, abi=self.abi
+        )
 
         lst_data = [18, 21]
         data = random.choice(lst_data)
 
-        vote_data = implementation_contract.functions.vote(data)._encode_transaction_data()
+        vote_data = implementation_contract.functions.vote(
+            data
+        )._encode_transaction_data()
 
         for _ in range(3):
             tx_hash = await self.client.send_transaction(
-                to=PROXY_CONTRACT_ADDRESS,
-                data=vote_data
+                to=PROXY_CONTRACT_ADDRESS, data=vote_data
             )
             if tx_hash:
-                logger.success(f"Успешно проголосовали!\nХэш транзакции: {self.client.network.explorer + tx_hash} | Адрес: {self.client.address}")
+                logger.success(
+                    f"Успешно проголосовали!\nХэш транзакции: {self.client.network.explorer + tx_hash} | Адрес: {self.client.address}"
+                )
             else:
-                logger.info(f"Не удалось проголосовать на {self.client.number} кошельке: {self.client.address}")
+                logger.info(
+                    f"Не удалось проголосовать на {self.client.number} кошельке: {self.client.address}"
+                )
 
             # Спим 2 минуты
             await asyncio.sleep(120)
