db55c83f9071b3483213e6c6c69acb2fa372c09e
munchen777.eth <isak.georgiy@mail.ru>
2024-09-10 00:06:58 +0300

    add capmonster api key

--- Plume/config.py
+++ Plume/config.py
@@ -3,3 +3,5 @@ from utils import read_list_from_file, get_proxies
 
 PRIVATE_KEYS = read_list_from_file(filepath="data/privates.txt")
 PROXIES: list[str] = get_proxies(filepath="data/proxies.txt")
+
+CAP_MONSTER_API: str = "YOUR CAP MONSTER API KEY"
